a4b28364cbeaa9a66fbab4d875a37093c75744a3
man liu <1711619194@qq.com>
2022-07-27 13:46:33 +0800

    write menu and playground scene

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -53,39 +53,3 @@ class AcGameMenu {
     }
 }
 
-class AcGamePlayground {
-    constructor(root) {
-        this.root = root;
-        this.$playground = $(`<div>游戏界面</div>`);
-
-        this.hide();
-        this.root.$ac_game.append(this.$playground);
-
-        this.start();
-    }
-
-    start() {
-    }
-
-    show() {  // 打开playground界面
-        this.$playground.show();
-    }
-
-    hide() {  // 关闭playground界面
-        this.$playground.hide();
-    }
-}
-
-class AcGame {
-    constructor(id) {
-        this.id = id;
-        this.$ac_game = $('#' + id);
-        this.menu = new AcGameMenu(this);
-        this.playground = new AcGamePlayground(this);
-
-        this.start();
-    }
-
-    start() {
-    }
-}
