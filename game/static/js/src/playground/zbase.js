875623bce11336920c2d2222db398713daa097b2
man liu <1711619194@qq.com>
2022-07-28 22:02:43 +0800

    write simple game

--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -1,17 +1,30 @@
 class AcGamePlayground {
     constructor(root) {
         this.root = root;
-        this.$playground = $(`<div>游戏界面</div>`);
+        this.$playground = $(`<div class="ac-game-playground"></div>`);
 
         this.hide();
         this.root.$ac_game.append(this.$playground);
-
+        this.width = this.$playground.width();
+        this.height = this.$playground.height();
+        this.game_map = new GameMap(this);
+        this.players = [];
+        this.players.push(new Player(this, this.width / 2, this.height / 2, this.height * 0.05, "white", this.height * 0.15, true));
+        for (let i = 0; i < 5; i ++ ) {
+                this.players.push(new Player(this, this.width / 2, this.height / 2, this.height * 0.05, this.get_random_color(), this.height * 0.15, false));
+        }
         this.start();
+    }
+    get_random_color() {
+        let colors = ["blue", "red", "pink", "grey", "green"];
+        return colors[Math.floor(Math.random() * 5)];
     }
 
     start() {
     }
 
+    update() {
+    }
     show() {  // 打开playground界面
         this.$playground.show();
     }
@@ -20,4 +33,3 @@ class AcGamePlayground {
         this.$playground.hide();
     }
 }
-
